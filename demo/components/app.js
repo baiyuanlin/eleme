e37a19daea72fd8eaa61f74b9c3fae84f48b20b2
xubaobao19940428 <915775317@qq.com>
2018-06-28 21:55:22 +0800

    钱诚

--- demo/components/app.js
+++ demo/components/app.js
@@ -1,5 +1,6 @@
 import React from  'react';
-
+import a from '../img/bg1.jpg'
+import b from '../img/1.png'
 import '../style/style.css'
 class App extends React.Component{
     constructor(props){
@@ -8,9 +9,24 @@ class App extends React.Component{
     render(){
         return(
             <div>
-                <h1 className="tit">hello</h1>
-                <p>哈哈哈</p>
-                <img src={require("../img/bg1.jpg")}/>
+                <header>
+                    <p>海为科技园</p>
+                    <input type="text" placeholder="输入商家、商品名称"/>
+                    <ul>
+                        <li>大盘鸡</li>
+                        <li>小龙虾</li>
+                        <li>黄焖鸡米饭</li>
+                        <li>烧烤</li>
+                        <li>粽子</li>
+                        <li>麻辣想过</li>
+                        <li>烤鱼</li>
+                        <li>酸菜鱼</li>
+                    </ul>
+                </header>
+                <banner>
+                    <img src={a}/>
+                    <img  className="img" src={b}/>
+                </banner>
             </div>
         )
 
