c46b7b5ef3b2921ce7190f39bab2d6a36a7ffc52
baiyuanlin <771095970@qq.com>
2018-06-28 22:06:51 +0800

    白远林模块

--- demo/components/app.js
+++ demo/components/app.js
@@ -8,9 +8,38 @@ class App extends React.Component{
     render(){
         return(
             <div>
-                <h1 className="tit">hello</h1>
-                <p>哈哈哈</p>
-                <img src={require("../img/bg1.jpg")}/>
+                
+                <div className="b-touw">
+                <div className="b-tou">
+                    <p className="shang">看不见</p>
+                    <p className="pz">——. 品质优选 .—— </p>
+                    <p className="gengduo">更多<span>></span> </p>
+                </div>
+                </div>
+                <div className="picw">
+                <div className="pic">
+                    <div>
+                        <img src={require("../img/1.jpg")}/>
+                        <p className ="shang">三米粥铺</p>
+                        <p className="xia">大牌精选</p>
+                    </div>
+                    <div>
+                        <img src={require("../img/2.jpg")}/>
+                        <p className ="shang">米多面多</p>
+                        <p className="xia">大牌精选</p>
+                    </div>
+                    <div>
+                        <img src={require("../img/3.jpg")}/>
+                        <p className ="shang">把愚便当</p>
+                        <p className="xia">大牌精选</p>
+                    </div>
+                    <div>
+                        <img src={require("../img/4.jpg")}/>
+                        <p className ="shang">阿利茄汁面</p>
+                        <p className="xia">大牌精选</p>
+                    </div>
+                </div>
+                </div>
             </div>
         )
 
